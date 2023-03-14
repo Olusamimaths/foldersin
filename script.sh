638912fb2718c6cca59db4e8409fbf1bb731449e
olusamimaths <olusola.samuel.oluwatobi@gmail.com>
2023-03-14 14:50:16 +0100

    first commit

---
+++ script.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+# This script deletes folders that doesn't contain files with a given extension
+# E.g if ext="xml", then this script will delete all folders that doesn't contain an xml file
+
+graceExtension="xml"
+count=0
+for DIR in *; do
+    [[ -d $DIR ]] || continue
+    for FILE in "$DIR"/*; do
+        [[ -f $FILE ]] || continue
+
+        if [[ $FILE =~ $graceExtension ]]; then
+            printf "\n▶ Skipping: Found xml file in %s\n" "$DIR"
+            break
+        fi
+        printf "☒ Deleting...: %s\n" "$DIR"
+        rm -rf "$DIR"
+        printf "\t ✗ ..deleted\n"
+        ((count += 1))
+    done
+done
+
+printf "\n"
+echo "→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→"
+printf "⌛ Deleted a total of %s folders\n" "$count"
+echo "→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→"
